218cacf0b26d0e65b8f1f96314cf0fe3c9c85587
g-mehra <gmehra@alumni.uwo.ca>
2017-01-30 15:05:07 -0500

    Complete assignments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,39 @@
 class PagesController < ApplicationController
 
+
+
   def welcome
+    @header = "This is the welcome page"
 
   end
 
   def about
-  end 
+  end
+
+  def contest
+    flash[:notice] = "Sorry, the contest has ended."
+    redirect_to welcome_path
+
+  end
+
+  def kitten
+    set_kitten_url
+  end
+
+  def kittens
+    set_kitten_url
+  end
+
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "https://unsplash.it/#{requested_size}"
+  end
+
+  def secrets
+    if params[:magic_word] != "elanore"
+      flash[:alert] = "Sorry, you're not authorized to see this page"
+      redirect_to root_url 
+    end
+  end
 
 end
